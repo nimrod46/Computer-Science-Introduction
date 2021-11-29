2955e74a1c04f1f4368fce5861e01e6c22a8b330
nimrod46 <nimrodma99@gmail.com>
2021-11-29 16:48:14 +0200

    Added lab 6 list comprehension

---
+++ lab_06/list_comprehension.py
@@ -0,0 +1,38 @@
+def multiply_lists(arr1, arr2):
+    return [arr1[i] * arr2[i] for i in range(len(arr1))]
+
+
+def mod_n_only(arr, n):
+    return [i for i in arr if i % n == 0]
+
+
+def how_many_below(arr, threshold):
+    return sum([1 if i < threshold else 0 for i in arr])
+
+
+def divide_list(arr, num_of_inner_lists):
+    return [[arr[i + j] for j in range(i, i + len(arr) // num_of_inner_lists)] for i in range(num_of_inner_lists)]
+
+
+def create_matrix(size):
+    return [[j for j in range(size)] for i in range(size)]
+
+
+def make_list(n):
+    return [[j for j in range(1, i + 1)] for i in range(1, n + 1)]
+
+
+def flatten_matrix(mat):
+    return [j for i in mat for j in i]
+
+
+def flatten_lst6(mat):
+    return [j for i in mat for j in i if len(j) < 6]
+
+
+def word_len_list(s, w):
+    return [len(i) for i in s.split() if i != w]
+
+
+def max_list(lst1, lst2):
+    return [lst1[i] if lst1[i] > lst2[i] else lst2[i] for i in range(len(lst1))]
---
+++ lab_06/testing/test_list_comprehension.py
@@ -0,0 +1,45 @@
+from lab_06.list_comprehension import *
+
+
+def test_multiply_lists():
+    assert multiply_lists([1, 2, 3], [1, 2, 3]) == [1, 4, 9]
+
+
+def test_mod_n_only():
+    assert mod_n_only([1, 2, 3, 4, 5, 6, 7], 3) == [3, 6]
+
+
+def test_how_many_below():
+    assert how_many_below([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5) == 4
+
+
+def test_divide_list():
+    assert divide_list([78, 57, 45, 4, 43, 45, 34, 12], 4) == [[78, 57], [45, 4], [43, 45], [34, 12]]
+
+
+def test_creat_matrix():
+    assert create_matrix(3) == [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
+
+
+def test_make_list():
+    assert make_list(3) == [[1], [1, 2], [1, 2, 3]]
+
+
+def test_flatten_matrix():
+    assert flatten_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def test_flatten_lst6():
+    assert flatten_lst6(
+        [["Mercury", "Venus", "Earth"], ["Mars", "Jupiter", "Saturn"], ["Uranus", "Neptune", "Pluto"]]) == ["Venus",
+                                                                                                            "Earth",
+                                                                                                            "Mars",
+                                                                                                            "Pluto"]
+
+
+def test_word_len_list():
+    assert word_len_list("the quick brown fox jumps over the lazy dog", "the") == [5, 5, 3, 5, 4, 4, 3]
+
+
+def test_max_list():
+    assert max_list([2, 1, 2, 1, 2, 1], [1, 2, 1, 2, 1, 2]) == [2, 2, 2, 2, 2, 2]

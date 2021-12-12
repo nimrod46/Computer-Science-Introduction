1b9c0d6b73b805f4840552f76ca22a6ea9bfb1eb
nimrod46 <nimrodma99@gmail.com>
2021-12-12 18:02:57 +0200

    Added docstring

--- ex4/no1/crypto.py
+++ ex4/no1/crypto.py
@@ -2,6 +2,11 @@ from random import *
 
 
 def is_key_legal(key):
+    """
+    Validating a given key
+    :param key: Key to validate
+    :return: True if the given key is legal, otherwise False
+    """
     if len(key) != 26:
         return False
     for c in key:
@@ -11,11 +16,21 @@ def is_key_legal(key):
 
 
 def generate_key():
+    """
+    Generates a random key
+    :return: Random key
+    """
     key = "".join([chr(ord("a") + randint(0, 25)) for i in range(26)])
     return key
 
 
 def encrypt(s, k):
+    """
+    Encrypt a given sting {s} by a given {k} using substitution cipher
+    :param s: Text to encrypt
+    :param k: Substitution cipher key
+    :return: Encrypted text
+    """
     s = str(s).lower()
     encryption_dict = dict((chr(ord("a") + i), k[i]) for i in range(len(k)))
     encrypt_text = ""
@@ -26,6 +41,12 @@ def encrypt(s, k):
 
 
 def decrypt(s, k):
+    """
+    Decrypt a given string "s" by a given "k" using substitution cipher
+    :param s: Text to Decrypt
+    :param k: Substitution cipher key
+    :return: Decrypted text
+    """
     s = str(s)
     decryption_dict = dict((k[i], chr(ord("a") + i)) for i in range(len(k)))
     decrypt_text = ""
@@ -35,6 +56,9 @@ def decrypt(s, k):
 
 
 def main():
+    """
+    Main logic func, asks the user for a command and creates the desired file containing the output
+    """
     command = input("please type the next command: \"e\" (encryption) or \"d\" (decryption): ")
     if command == "e":
         key = generate_key()
@@ -55,30 +79,41 @@ def main():
 
 
 def decrypt_from_user(cipher_file_name="ciphertext.txt", key_file_name="key.txt"):
-    key = ""
+    """
+    Decrypt a cipher file by a given key
+    :param cipher_file_name: For testing, an optional input cipher file name
+    :param key_file_name: For testing, an optional input key file name
+    :return: The decrypted text
+    """
     try:
         with open(key_file_name, "r") as key_file:
             key = key_file.readline()
             if not is_key_legal(key):
                 print(f"Key in \"{key_file_name}\" is illegal!")
                 return False
-        decrypted_lines = ""
+        decrypted_text = ""
         with open(cipher_file_name, "r") as input_file:
             for line in input_file.readlines():
-                decrypted_lines += (decrypt(line, key))
-        return decrypted_lines
+                decrypted_text += (decrypt(line, key))
+        return decrypted_text
     except IOError as e:
         print(f"Could not open file \"{e.filename}\"!")
         return False
 
 
 def encrypt_from_user(key, plaintext_file_name="plaintext.txt"):
-    encrypted_lines = ""
+    """
+    Encrypt plain text file using a given key
+    :param key: A given key to encrypt with
+    :param plaintext_file_name: For testing, an optional input text file name
+    :return: The encrypted text
+    """
+    encrypted_text = ""
     try:
         with open(plaintext_file_name, "r") as input_file:
             for line in input_file.readlines():
-                encrypted_lines += (encrypt(line, key))
-        return encrypted_lines
+                encrypted_text += (encrypt(line, key))
+        return encrypted_text
     except IOError as e:
         print(f"Could not open file \"{e.filename}\"!")
         return False
--- ex4/no2/grades.py
+++ ex4/no2/grades.py
@@ -1,19 +1,32 @@
-import math
 
 
 def students_read():
+    """
+    Reads students names from file and splitting the data to a dictionary
+    :return: Dictionary from student's ids to name
+    """
     with open("students.txt", "r") as students_file:
         dic = {line.split()[0]: " ".join(line.split()[1:]) for line in students_file.readlines()}
-        return dic
+    return dic
 
 
 def grades_read():
+    """
+    Reads student's grades from file and splitting the data to a dictionary
+    :return: Dictionary from student's ids to grades
+    """
     with open("grades.txt", "r") as students_file:
         dic = {line.split()[0]: list(map(int, line.split()[1:])) for line in students_file.readlines()}
-        return dic
+    return dic
 
 
 def max_avg_student(students_names_by_id, students_grades_by_id):
+    """
+    Finds the student with the highest grades average
+    :param students_names_by_id: Student's names by id dictionary
+    :param students_grades_by_id: Student's grades by id dictionary
+    :return: The name and the average grades of the highest student
+    """
     max_avg = (0, "")
     for student in students_names_by_id:
         avg = sum(students_grades_by_id[student]) / len(students_grades_by_id[student])
@@ -23,6 +36,12 @@ def max_avg_student(students_names_by_id, students_grades_by_id):
 
 
 def max_grades_count(students_names_by_id, students_grades_by_id):
+    """
+    Finds the grades that appeared the most in student's grades
+    :param students_names_by_id: Student's names by id dictionary
+    :param students_grades_by_id: Student's grades by id dictionary
+    :return: All the grades the appeared the most and their count
+    """
     grades_count_by_grades = {}
     for student in students_names_by_id:
         for grade in students_grades_by_id[student]:
@@ -31,18 +50,25 @@ def max_grades_count(students_names_by_id, students_grades_by_id):
             else:
                 grades_count_by_grades[grade] = 1
 
-    max_grade = [(0, 0)]
+    max_grade = []
+    count = 0
     for grade in grades_count_by_grades:
-        if grades_count_by_grades[grade] == max_grade[0][1]:
-            max_grade.append((grade, grades_count_by_grades[grade]))
-        if grades_count_by_grades[grade] > max_grade[0][1]:
+        if grades_count_by_grades[grade] == count:
+            max_grade.append(grade)
+            count = grades_count_by_grades[grade]
+        if grades_count_by_grades[grade] > count:
             max_grade.clear()
-            max_grade = [(0, 0)]
-            max_grade[0] = (grade, grades_count_by_grades[grade])
-    return max_grade
+            max_grade.append(grade)
+            count = grades_count_by_grades[grade]
+    return max_grade, count
 
 
 def grades_not_presents(students_grades_by_id):
+    """
+    Finds the grades (from 0  to 100) that did not appear in the student's grades
+    :param students_grades_by_id: Student's grades by id dictionary
+    :return: Missing grades
+    """
     grades = {i for i in range(101)}
     for student_grades in students_grades_by_id.values():
         for grade in student_grades:
@@ -52,6 +78,10 @@ def grades_not_presents(students_grades_by_id):
 
 
 def validate_students_names_by_id(students_names_by_id):
+    """
+    :param students_names_by_id: Student's names by id dictionary
+    :return: True if id and names are valid, otherwise, False
+    """
     for student_id in students_names_by_id:
         if not validate_id(student_id) or students_names_by_id[student_id] == "":
             return False
@@ -59,6 +89,10 @@ def validate_students_names_by_id(students_names_by_id):
 
 
 def validate_students_grades_by_id(students_grades_by_id):
+    """
+    :param students_grades_by_id: Student's grades by id dictionary
+    :return: True if id and grades are valid, otherwise, False
+    """
     for student_id in students_grades_by_id:
         if not validate_id(student_id) or students_grades_by_id[student_id] == []:
             return False
@@ -66,32 +100,52 @@ def validate_students_grades_by_id(students_grades_by_id):
 
 
 def validate_id(student_id):
+    """
+    :param student_id: A student id
+    :return: True if the student id is a valid id, otherwise False
+    """
     return len(student_id) == 9
 
 
 def main():
+    """
+    Main logic func: reads students data, validating the data and printing:
+    The student with the highest average grade
+    The most appearing grades
+    List of all the grades that did not appear in any student
+    """
     try:
         students_names_by_id = students_read()
+    except IOError as e:
+        print(f"Could not open file: {e.filename}")
+        return
+    except Exception:
+        print("Invalid data in student's names file")
+        return
+    try:
         students_grades_by_id = grades_read()
-    except Exception as e:
-        print("Error reading students data: " + str(e))
+    except IOError as e:
+        print(f"Could not open file: {e.filename}")
+        return
+    except Exception:
+        print("Invalid data in student's grades file")
         return
 
     if not validate_students_names_by_id(students_names_by_id):
-        print("Failed validating student's names data")
+        print("Invalid data in student's names file")
         return
     if not validate_students_grades_by_id(students_grades_by_id):
-        print("Failed validating student's grades data")
+        print("Invalid data in student's grades file")
         return
     if students_names_by_id.keys() != students_grades_by_id.keys():
         print("Some student's ids are missing!")
         return
 
     max_avg = max_avg_student(students_names_by_id, students_grades_by_id)
-    print(f"Best student: {max_avg[1]}, average: {max_avg[0]}")
+    print(f"Best student: {max_avg[1]}, average: {max_avg[0]:.2f}")
 
-    max_grades = max_grades_count(students_names_by_id, students_grades_by_id)
-    print(f"The grade {', '.join([str(t[0]) for t in max_grades])} appeared {max_grades[0][1]} times")
+    max_grades, count = max_grades_count(students_names_by_id, students_grades_by_id)
+    print(f"The grade {', '.join([str(t) for t in max_grades])} appeared {count} times")
 
     missing_grades = grades_not_presents(students_grades_by_id)
     print(f"The grades that did not appear:")
